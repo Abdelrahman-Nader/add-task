14e37a506b157d974b73aff0ba53e8c60c7ab1d0
Abdelrahaman Nader <abualiya.2202@gmail.com>
2022-06-30 12:42:29 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { AddTaskComponent } from './component/add-task/add-task.component';
+import { TaskComponent } from './component/task/task.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AddTaskComponent,
+    TaskComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
