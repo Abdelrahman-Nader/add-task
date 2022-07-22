e499d59f2caa9881fe883456fecc2132b7569ad4
“abdo” <abualiya.2202@gmail.com>
2022-07-22 15:40:28 +0200

    bootstrabfrom home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AddTaskComponent } from './component/add-task/add-task.component';
 import { TaskComponent } from './component/task/task.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
